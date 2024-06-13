a1db529c4864f1381e54020d5b4d3923b33bec23
Charlie-Steer <gruiz.carlos@gmail.com>
2024-06-13 20:08:46 +0200

    Progress on printf.c. Now Makefile needed.

--- printf/printf.c
+++ printf/printf.c
@@ -6,23 +6,75 @@
 /*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 14:05:37 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/06/13 14:05:39 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/06/13 19:39:219 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include <stdlib.h>
 #include <stdarg.h>
 
-int printf(char *str, ...)
+static void print_format_specifier(char *str, va_list args)
+{
+    if (*str == 'c')
+        ft_putchar_fd(va_arg(args, int), 1);
+    else if (*str == 's')
+        ft_putstr_fd(va_arg(args, char *), 1);
+    else if (*str == 'd' || *str == 'i')
+        ft_putnbr_fd(va_arg(args, int), 1);
+    else if (*str == 'p')
+    {
+
+    }
+    else if (*str == 'u')
+    {
+
+    }
+    else if (*str == 'x')
+    {
+
+    }
+    else if (*str == 'X')
+    {
+
+    }
+    else if (*str == '%')
+        ft_putchar_fd('%', 1);
+}
+
+void ft_printf(char *str, ...)
 {
     va_list args;
+    void    *arg;
+    int     str_len;
+    int     i;
 
     va_start(args, str);
 
-
+    str_len = strlen(str);
+    
+    while (i < str_len)
+    {
+        if (*str != '%')
+        {
+            ft_putchar_fd(*str, 1);
+            i++;
+            continue ;
+        }
+        i++;
+        if (*str == '%')
+            ft_putchar_fd(*str, 1);
+        else
+        {
+            void *test = (void *)7;
+            print_format_specifier(str, args);
+        }
+    }
+    va_end(args);
 }
 
 int main()
 {
-
+    ft_printf("hello %c world", 'y');
+    return (0);
 }
