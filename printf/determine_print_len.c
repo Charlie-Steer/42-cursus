07581444d17171016e92f57099199f6bb951b459
Charlie-Steer <gruiz.carlos@gmail.com>
2024-07-29 21:30:05 +0200

    Didn't pass Moulinette evaluation. Results seem pretty catastrophic. Matias caught an error on the return value of NULL pointers. It has been fixed. Now I should try to run Tripouille's printfTester and maybe convert my francinette tests into my custom test function tests, as maybe the francinette tests didn't take return values into account and my test function does. Miguel also mentioned that write() should return -1 if the write fails and that isn't being handled by my project.

--- printf/determine_print_len.c
+++ printf/determine_print_len.c
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 13:26:56 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/07/25 16:31:19 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/07/29 21:22:59 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,10 @@ static int	get_print_len_x(t_conv_spec cs, int print_len)
 	return (print_len);
 }
 
-static int	get_print_len_p(t_conv_spec cs, int print_len)
+static int	get_print_len_p(t_conv_spec cs, int print_len, char *n_str)
 {
+	if (!ft_strncmp("(nil)", n_str, 6))
+		return (print_len);
 	if (cs.point_width > print_len)
 		print_len = cs.point_width;
 	print_len += 2;
@@ -73,6 +75,6 @@ int	determine_cs_print_len(char *n_str, t_conv_spec cs)
 	else if (cs.conv_specifier == 'x' || cs.conv_specifier == 'X')
 		print_len = get_print_len_x(cs, print_len);
 	else if (cs.conv_specifier == 'p')
-		print_len = get_print_len_p(cs, print_len);
+		print_len = get_print_len_p(cs, print_len, n_str);
 	return (print_len);
 }
