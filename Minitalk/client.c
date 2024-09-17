b16aced0ede16cb947e85a9ff1cf30bd053047ff
Charlie-Steer <gruiz.carlos@gmail.com>
2024-09-17 21:59:15 +0200

    Continuing with Minitalk. Must bring in libft functions.

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:37:03 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/16 20:55:49 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/09/17 21:04:36 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,33 @@
 
 int main(int argc, char **argv)
 {
-	//! Seems like we have to handle argv[i] possibly not existing.
-	//! Otherwise a segmentation fault happens.
+	if (argc != 2 || argc != 3) //! Improve imput validation.
+		return (printf("Error: Invalid argument count.\n"), 1); //! Careful with stdio printf.
 	int server_pid = atoi(argv[1]); //! CAREFUL! Non-ft atoi.
-	kill(server_pid, SIGUSR1);
+	// kill(server_pid, SIGUSR1);
+	{
+		char *message = argv[2]; //! Properly copy the contents to a malloc'd message.
+		int message_len = ft_strlen(message);
+		char bit_value = 0;
+		char mask = 1;
+		int char_index = 0;
+		while (char_index < message_len)
+		{
+			int bit_index = 0;
+			while (bit_index < 8)
+			{
+				bit_value = message[bit_index] & mask;
+				if (bit_value == 0)
+					kill(server_pid, SIGUSR1);
+				else
+					kill(server_pid, SIGUSR2);
+				message[char_index] >>= 1;
+				bit_index++;
+			}
+			bit_index = 0;
+			char_index++;
+		}
+	}
 
 	return (0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,28 +6,55 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:23:22 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/16 20:53:44 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/09/17 21:21:21 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h> //! Careful with this dude.
 #include <unistd.h>
+#include <stdlib.h>
 
-void	sigusr1_handler()
+volatile sig_atomic_t a;
+
+void	sigusr1_handler(int signum) //! I think only atomic operations should go inside a handler.
+{
+	if (signum == SIGUSR1)
+	{
+		a |= 0;
+		a <<= 1;
+	}
+	else if (signum == SIGUSR2)
+	{
+		a |= 1;
+		a <<= 1;
+	}
+}
+
+void	sigusr2_handler()
 {
 	printf("Signal received!\n");
 }
 
 int	main(void)
 {
+	a = 0;
+	printf("%lu\n", sizeof(sig_atomic_t));
 	signal(SIGUSR1, sigusr1_handler);
+	signal(SIGUSR2, sigusr1_handler);
+	// message = calloc(42, 1);
 	int pid = getpid();
 	printf("Server PID: %d\n", pid);
+	printf("Awaiting message...");
 	while (1)
 	{
-		printf("Waiting...\n");
-		sleep(1);
+		int i = 0;
+		while (i < 8)
+		{
+			pause();
+			i++;
+		}
+		i = 0;
 	}
 	return (0);
 }
