c2f100e34716ff319741da43187faca9a25c7aa5
Charlie-Steer <gruiz.carlos@gmail.com>
2024-10-01 00:36:04 +0200

    Minitalk progress. Signals are sending and receiving alright. The server global variable is seemingly not maintaining a value. Possibly a misunderstanding on bitshifting behaviour on my part.

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,18 +6,18 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:37:03 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/26 20:53:07 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/09/30 13:15:36 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <assert.h>
 
 int main(int argc, char **argv)
 {
-	if (argc != 2 || argc != 3) //! Improve imput validation.
+	if (argc != 2 && argc != 3) //? How should it act on argc == 2?
 		return (ft_printf("Error: Invalid argument count.\n"), 1);
 	int server_pid = ft_atoi(argv[1]);
-	// kill(server_pid, SIGUSR1);
 	{
 		char *message = argv[2]; //! Properly copy the contents to a malloc'd message.
 		int message_len = ft_strlen(message);
@@ -29,13 +29,23 @@ int main(int argc, char **argv)
 			int bit_index = 0;
 			while (bit_index < 8)
 			{
-				bit_value = message[bit_index] & mask;
+				// printf("message: %s\n", message);
+				bit_value = message[char_index] & mask;
+				// printf("bit_value: %d\n", bit_value);
 				if (bit_value == 0)
+				{
+					printf("0!\n");
 					kill(server_pid, SIGUSR1);
+				}
 				else
+				{
+					printf("1!\n");
+					assert(bit_value == 1); //! remove
 					kill(server_pid, SIGUSR2);
+				}
 				message[char_index] >>= 1;
 				bit_index++;
+				usleep(1000);
 			}
 			bit_index = 0;
 			char_index++;
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,51 +6,60 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:23:22 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/18 14:04:43 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/09/30 13:29:27 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile sig_atomic_t a;
+// volatile sig_atomic_t a;
+volatile char a;
 
 void	sigusr1_handler(int signum) //! I think only atomic operations should go inside a handler.
 {
+	// ft_printf("0\n");
+	// ft_printf("test:\n");
 	if (signum == SIGUSR1)
 	{
+		// ft_printf("here\n");
 		a |= 0;
-		a <<= 1;
+		ft_printf("a: %d\n", a);
+		a >>= 1;
 	}
 	else if (signum == SIGUSR2)
 	{
+		// ft_printf("or here?\n");
 		a |= 1;
-		a <<= 1;
+		ft_printf("a: %d\n", a);
+		a >>= 1;
 	}
 }
 
-void	sigusr2_handler()
+void	sigusr2_handler(int signum)
 {
-	printf("Signal received!\n");
+	ft_printf("1\n");
 }
 
 int	main(void)
 {
 	a = 0;
-	ft_printf("%lu\n", sizeof(sig_atomic_t));
+	// ft_printf("%lu\n", sizeof(sig_atomic_t));
 	signal(SIGUSR1, sigusr1_handler);
 	signal(SIGUSR2, sigusr1_handler);
 	// message = calloc(42, 1);
 	int pid = getpid();
-	printf("Server PID: %d\n", pid);
-	printf("Awaiting message...");
+	ft_printf("Server PID: %d\n", pid);
+	ft_printf("Awaiting message...");
 	while (1)
 	{
 		int i = 0;
 		while (i < 8)
 		{
 			pause();
+			ft_printf("a is now: %c\n", a);
 			i++;
 		}
+		ft_printf("char: %c\n", a);
 		i = 0;
 	}
 	return (0);
