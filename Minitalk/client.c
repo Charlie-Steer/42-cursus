2d7efb9dfdca0b8d227c45fb24046811648626ad
Charlie-Steer <gruiz.carlos@gmail.com>
2024-10-02 19:58:56 +0200

    I seem to have improved performance. Still it fails at 300 microseconds between signal sends sometimes. 400 seems to work well. I think it should be fine to deliver. Make any Norm fixes, including libft just in case.

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:37:03 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/10/02 14:46:10 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:56:39 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,28 @@ void	send_message(int server_pid, char *message)
 
 	while (char_index < message_len)
 	{
+		// ft_printf("%c	", message[char_index]);
+		// ft_printf(" (%d)	", message[char_index]);
 		while (bit_index < 8)
 		{
-			bit_value = message[char_index] & mask;
-			bit_value >>= 7;
+			bit_value = (unsigned char)(message[char_index]) & mask;
 			if (bit_value == 0)
 			{
-				ft_printf("0\n");
+				// printf("0");
 				kill(server_pid, SIGUSR1);
 			}
 			else
 			{
-				assert(bit_value == 1); //! remove
-				printf("1\n");
+				// assert(bit_value == 1); //! remove
+				// printf("1");
 				kill(server_pid, SIGUSR2);
 			}
+			// if (bit_index == 7)
+			// 	printf("\n");
 			message[char_index] <<= 1;
 			bit_index++;
 			// ft_printf("\n");
-			usleep(150);
+			usleep(500);
 		}
 		bit_index = 0;
 		char_index++;
@@ -54,7 +57,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 2 && argc != 3) //? How should it act on argc == 2?
 		return (ft_printf("Error: Invalid argument count.\n"), 1);
-	int server_pid = ft_atoi(argv[1]);
+	int server_pid = ft_atoi(argv[1]); //! FT!
 
 	//? Properly copy the contents to a malloc'd message?
 	g_str = argv[2];
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,48 +6,105 @@
 /*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:23:22 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/10/01 16:16:27 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:44:50 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <signal.h>
 
-volatile sig_atomic_t c;
-// volatile char c;
+// volatile sig_atomic_t c;
+//volatile unsigned char c;
 
-void	sigusr1_handler(int signum) //! I think only atomic operations should go inside a handler.
+//void	sigusr1_handler(int signum) //! I think only atomic operations should go inside a handler.
+//{
+	//c |= 0;
+//}
+
+//void	sigusr2_handler(int signum)
+//{
+	//c |= 1;
+//}
+
+void	alt_handler(int signum)
 {
-	c |= 0;
-}
+	static unsigned char	c;
+	static int				i = 1;
+	// DEBUG 1
+	// {
+	// 	if (i == 1)
+	// 		printf("		");
+	// }
 
-void	sigusr2_handler(int signum)
-{
-	c |= 1;
+	if (signum == SIGUSR1)
+	{
+		// printf("0");
+		c |= 0;
+	}
+	else
+	{
+		// printf("1");
+		c |= 1;
+	}
+
+	if (i == 8)
+	{
+		// DEBUG 2
+		// {
+		// 	printf("\n");
+		// 	printf("%c		", c);
+		// 	for (int j = 1; j <= 8; j++)
+		// 	{
+		// 		if ((c & (1 << (8 - j))) == 0)
+		// 			printf("0");
+		// 		else
+		// 			printf("1");
+		// 	}
+		// 	printf("\n\n");
+		// }
+		write(1, (void *)&c, 1);
+		i = 1;
+		c = 0;
+		return ;
+	}
+	else
+	{
+		c <<= 1;
+		i++;
+	}
 }
 
 int	main(void)
 {
-	c = 0;
+	//c = 0;
 	// ft_printf("%lu\n", sizeof(sig_atomic_t));
-	signal(SIGUSR1, sigusr1_handler);
-	signal(SIGUSR2, sigusr2_handler);
-	// message = calloc(42, 1);
+	// signal(SIGUSR1, alt_handler);
+	// signal(SIGUSR2, alt_handler);
+
+	struct sigaction sa; //? Can't declare here?
+	sa.sa_handler = &alt_handler;
+	sa.sa_flags = SA_RESTART;
+
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	int pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
 	ft_printf("Awaiting message...\n");
 	int i = 0;
+	// while (1)
+	// {
+	// 	while (i < 8)
+	// 	{
+	// 		pause();
+	// 		if (i != 7)
+	// 			c <<= 1;
+	// 		i++;
+	// 	}
+	// 	write(1, (void *)&c, 1);
+	// 	c = 0;
+	// 	i = 0;
+	// }
 	while (1)
-	{
-		while (i < 8)
-		{
-			pause();
-			if (i != 7)
-				c <<= 1;
-			i++;
-		}
-		write(1, (void *)&c, 1);
-		c = 0;
-		i = 0;
-	}
+		pause();
 	return (0);
 }
