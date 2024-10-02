ca43fa44521f9b6badeacd276f5bb226aecbb414
Charlie-Steer <gruiz.carlos@gmail.com>
2024-10-02 13:49:59 +0200

    Commit from previous day changes. App seems to be working right for the most part, except that sometimes things seem to mess up and permanently change the state of the server application. Unsure whether you this is a fixable issue.

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -3,54 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:37:03 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/30 13:15:36 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:27:31 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <assert.h>
 
+void	send_bit()
+{
+
+}
+
+void	send_message(int server_pid, char *message)
+{
+	int				message_len = ft_strlen(message);
+	unsigned char	bit_value = 0;
+	unsigned char	mask = 128;
+	int				char_index = 0;
+	int				bit_index = 0;
+
+	while (char_index < message_len)
+	{
+		while (bit_index < 8)
+		{
+			bit_value = message[char_index] & mask;
+			bit_value >>= 7;
+			if (bit_value == 0)
+			{
+				ft_printf("0");
+				kill(server_pid, SIGUSR1);
+			}
+			else
+			{
+				assert(bit_value == 1); //! remove
+				printf("1");
+				kill(server_pid, SIGUSR2);
+			}
+			message[char_index] <<= 1;
+			bit_index++;
+			ft_printf("\n");
+			usleep(200);
+		}
+		bit_index = 0;
+		char_index++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2 && argc != 3) //? How should it act on argc == 2?
 		return (ft_printf("Error: Invalid argument count.\n"), 1);
 	int server_pid = ft_atoi(argv[1]);
-	{
-		char *message = argv[2]; //! Properly copy the contents to a malloc'd message.
-		int message_len = ft_strlen(message);
-		char bit_value = 0;
-		char mask = 1;
-		int char_index = 0;
-		while (char_index < message_len)
-		{
-			int bit_index = 0;
-			while (bit_index < 8)
-			{
-				// printf("message: %s\n", message);
-				bit_value = message[char_index] & mask;
-				// printf("bit_value: %d\n", bit_value);
-				if (bit_value == 0)
-				{
-					printf("0!\n");
-					kill(server_pid, SIGUSR1);
-				}
-				else
-				{
-					printf("1!\n");
-					assert(bit_value == 1); //! remove
-					kill(server_pid, SIGUSR2);
-				}
-				message[char_index] >>= 1;
-				bit_index++;
-				usleep(1000);
-			}
-			bit_index = 0;
-			char_index++;
-		}
-	}
+
+	//? Properly copy the contents to a malloc'd message?
+	send_message(server_pid, argv[2]);
 
 	return (0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -3,63 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:23:22 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/09/30 13:29:27 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/10/01 16:16:27 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// volatile sig_atomic_t a;
-volatile char a;
+volatile sig_atomic_t c;
+// volatile char c;
 
 void	sigusr1_handler(int signum) //! I think only atomic operations should go inside a handler.
 {
-	// ft_printf("0\n");
-	// ft_printf("test:\n");
-	if (signum == SIGUSR1)
-	{
-		// ft_printf("here\n");
-		a |= 0;
-		ft_printf("a: %d\n", a);
-		a >>= 1;
-	}
-	else if (signum == SIGUSR2)
-	{
-		// ft_printf("or here?\n");
-		a |= 1;
-		ft_printf("a: %d\n", a);
-		a >>= 1;
-	}
+	c |= 0;
 }
 
 void	sigusr2_handler(int signum)
 {
-	ft_printf("1\n");
+	c |= 1;
 }
 
 int	main(void)
 {
-	a = 0;
+	c = 0;
 	// ft_printf("%lu\n", sizeof(sig_atomic_t));
 	signal(SIGUSR1, sigusr1_handler);
-	signal(SIGUSR2, sigusr1_handler);
+	signal(SIGUSR2, sigusr2_handler);
 	// message = calloc(42, 1);
 	int pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
-	ft_printf("Awaiting message...");
+	ft_printf("Awaiting message...\n");
+	int i = 0;
 	while (1)
 	{
-		int i = 0;
 		while (i < 8)
 		{
 			pause();
-			ft_printf("a is now: %c\n", a);
+			if (i != 7)
+				c <<= 1;
 			i++;
 		}
-		ft_printf("char: %c\n", a);
+		write(1, (void *)&c, 1);
+		c = 0;
 		i = 0;
 	}
 	return (0);
