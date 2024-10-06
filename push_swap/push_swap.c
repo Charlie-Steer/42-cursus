9b3b2b5e2766d3f0e64e276e9d47edb9509c4de0
Charlie-Steer <gruiz.carlos@gmail.com>
2024-10-06 18:22:19 +0200

    Started push_swap.

---
+++ push_swap/push_swap.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 17:41:52 by cargonz2          #+#    #+#             */
+/*   Updated: 2024/10/06 18:18:40 by cargonz2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int	number_of_strings(char **char_array)
+{
+	int	count;
+
+	count = 0;
+	while (char_array[count])
+		count++;
+	return (count);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) //? Should argc !=2 behave differently?
+		ft_printf("Error: invalid argument count.\n");
+
+	char **number_strings = ft_split(argv[1], ' ');
+	char **list_a = ft_calloc(number_of_strings(number_strings), sizeof(int));
+	if (!list_a)
+		return (1);
+	int	i = 0;
+	while (number_strings[i])
+	{
+		int *number = ft_calloc(1, sizeof(int));
+		if (!number)
+			return (1); //! Should free all allocated numbers so far.
+
+		*number = ft_atoi(number_strings[i]);
+		ft_lstnew((void *)number);
+		i++;
+	}
+	argv[1];
+
+	return (0);
+}
