dec026562bcaba874f5772d8abcae2d9729b69e8
Charlie-Steer <gruiz.carlos@gmail.com>
2024-11-19 17:34:47 +0100

    Checked for width and height of the map to malloc the dimensions necessary to save the map and saved the map. Now on to extract information and perform checks on the saved map.

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:11:39 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/11/18 17:23:19 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:36:39 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,18 @@
 #include <assert.h>
 #include <stdbool.h>
 
+typedef struct s_two_ints {
+	int a;
+	int b;
+} t_two_ints;
+
 typedef struct s_map_data {
-    int width;
-    int height;
-    int collectible_amount;
+	int width;
+	int height;
+
+	int player_amount;
+	int exit_amount;
+	int collectible_amount;
 } t_map_data;
 
 #endif
